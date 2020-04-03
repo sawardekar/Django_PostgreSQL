e4ce83f4366041f3cfb5a96d0cdad5c4af97e0e8
sawardekar <sanketsawardekar97@gmail.com>
2020-04-03 16:47:34 +0530

    first commit

---
+++ employee/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from employee.models import Employee, EmployeeType, Product
+# Register your models here.
+
+admin.site.register(Employee)
+admin.site.register(EmployeeType)
+admin.site.register(Product)
+
